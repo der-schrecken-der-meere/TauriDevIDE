10e31913fa6aa88f392ad6e142aee67572e1728d
Max Voigt <max12falkensee@gmail.com>
2024-11-08 11:23:44 +0100

    initialize project and dependencies

---
+++ src-tauri/gen/android/app/src/main/java/com/tauri_dev_ide/app/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.tauri_dev_ide.app
+
+class MainActivity : TauriActivity()
---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+const host = process.env.TAURI_DEV_HOST;
+
+// https://vitejs.dev/config/
+export default defineConfig(async () => ({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    }
+  },
+
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
+  build: {
+    target: process.env.TAURI_ENV_PLATFORM == 'windows' ? 'chrome132' : 'safari18',
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+    // minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false
+  },
+}));
